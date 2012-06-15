8a6ea784c1abaa6296c7a7f094c2b3100bdd581f
Ben <opbenesh@gmail.com>
2012-06-15 18:55:10 +0300

    Added project to source control.
    Contains: basic WPF GUI and some early logic.

---
+++ MusicQuiz.GUI/Logic.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MusicQuiz.GUI
+{
+    public class QuizDB
+    {
+        enum Subject
+        {
+            Artist=0,
+            Album,
+            Title
+        }
+        private const int SUBJECTS_COUNT = 3;
+        private const int TRACKS_COUNT = 100;
+        private const int OPTIONS_COUNT = 4;
+
+        private List<TagLib.File> _files;
+        private Random _random;
+
+        public QuizDB()
+        {
+            _files = new List<TagLib.File>();
+            var musicDir = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            var files = Directory.GetFiles(musicDir, "*.mp3");
+            for (int i = 0; i < TRACKS_COUNT;)
+            {
+                try
+                {
+                    _files.Add(TagLib.File.Create(files[TRACKS_COUNT]));
+                    i++;
+                }
+                catch { }
+            }
+        }
+
+        public Question CreateNewQuestion()
+        {
+            var files = new List<TagLib.File>();
+            for (int i = 0; i < OPTIONS_COUNT; i++)
+            {
+                files.Add(_files[_random.Next(_files.Count)]);
+            }
+            var subject = (Subject)_random.Next(SUBJECTS_COUNT);
+            List<string> rawOptions = null;
+            string title= string.Empty;
+            switch (subject)
+            {
+                case Subject.Artist:
+                    rawOptions = files.Select(f => f.Tag.Performers[0]).ToList();
+                    title = "Which artist perform this track?";
+                    break;
+                case Subject.Album:
+                    rawOptions = files.Select(f => f.Tag.Album).ToList();
+                    title = "Which album contains this track?";
+                    break;
+                case Subject.Title:
+                    rawOptions = files.Select(f => f.Tag.Title).ToList();
+                    title = "What is the title of this track?";
+                    break;
+            }
+            var options = rawOptions.Select(s => new Option() { Title = s, IsCorrect = false }).ToList();
+            options[_random.Next(OPTIONS_COUNT)].IsCorrect = true;
+            return new Question()
+                {
+                    Title=title,
+                    Options=options
+                };
+        }
+    }
+}
---
+++ MusicQuiz.GUI/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicQuiz.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private QuestionVM _currentQuestion;
+
+        private const int QUESTION_VALUE = 10;
+        private int _score = 0;
+
+        public MainWindow()
+        {
+            var q = new Question()
+            {
+                Title = "Which artist?",
+                Options = new List<Option>()
+                {
+                    new Option(){Title="Pink Floyd", IsCorrect=false},
+                    new Option(){Title="The Beatles", IsCorrect=false},
+                    new Option(){Title="Nirvana", IsCorrect=true},
+                    new Option(){Title="Led Zeppelin", IsCorrect=false}
+                }
+            };
+            this.DataContext = _currentQuestion = new QuestionVM(q);
+            InitializeComponent();
+        }
+
+
+        private void CloseCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OptionsLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!_currentQuestion.IsActive || e.AddedItems.Count == 0) return;
+            var option = e.AddedItems.Cast<OptionVM>().Single() as OptionVM;
+            if (option == null)
+                return;
+            if ((option.IsUserCorrect = option.Option.IsCorrect).Value)
+            {
+                _score += QUESTION_VALUE;
+            }
+            _currentQuestion.IsActive = false;
+            //ChangeQuestion();
+        }
+
+        private void ChangeQuestion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Question
+    {
+        public string Title { get; set; }
+
+        public List<Option> Options { get; set; }
+    }
+
+    class QuestionVM
+    {
+        public QuestionVM(Question question)
+        {
+            this.IsActive = true;
+            this.Options = question.Options.Select(o => new OptionVM(o)).ToList();
+        }
+
+        public List<OptionVM> Options { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+
+    public class Option
+    {
+        public string Title { get; set; }
+
+        public bool IsCorrect { get; set; }
+    }
+
+    class OptionVM:INotifyPropertyChanged
+    {
+        public OptionVM(Option option)
+        {
+            this.Option = option;
+        }
+
+        private Option _option;
+        public Option Option
+        {
+            get
+            {
+                return _option;
+            }
+            set
+            {
+                _option = value;
+                NotifyPropertyChanged("Option");
+            }
+        }
+
+        private bool? _isUserCorrect;
+        public bool? IsUserCorrect
+        {
+            get
+            {
+                return _isUserCorrect;
+            }
+            set
+            {
+                _isUserCorrect = value;
+                NotifyPropertyChanged("IsUserCorrect");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
